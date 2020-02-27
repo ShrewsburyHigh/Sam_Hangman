8e5c25bae447133097e475c6afc7e3e6e512c7e1
pgpali20 <60894829+pgpali20@users.noreply.github.com>
2020-02-27 13:21:02 -0500

    Update hangman.py
    New part added

--- hangman.py
+++ hangman.py
@@ -1,6 +1,33 @@
 from __future__ import print_function
 import random
 lives = 6
+word = ""
+letter = ""
+updatedSpaces = []
+def initialize():
+    global word
+    global updatedSpaces
+    word = "airport"
+    print ("_ " * len(word))
+    updatedSpaces = ("_ " * len(word))
+    print("try to guess the word in less than 6 tries")
+    print(updatedSpaces)
+def getLetter():
+    global letter
+    print ("What is your guess")
+    letter = raw_input()
+#def check():        
+def win():
+    if updatedSpaces == word:
+        print("you won the game!")
+    else:
+        if lives == 0:
+            print("you lost")
+def main():
+    initialize()
+    getLetter()
+    #check()
+lives = 6
 words = "hello"
 letter = " "
 updatedspaces = ['_', '_', '_', '_', '_']
